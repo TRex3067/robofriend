df887a2585e4c0cf23f720f5338e889e2084ded6
TRex3067 <56078802+TRex3067@users.noreply.github.com>
2020-04-21 10:16:31 +0530

    Robo App

---
+++ precache-manifest.a118216f62f65e74da62b462af143bd4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3e68a97838a59ffa91e5e01ce12426",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dad70c6b4f393277e7d2",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "6ab422193c9d9ecc0c39",
+    "url": "/static/css/main.295b049c.chunk.css"
+  },
+  {
+    "revision": "dad70c6b4f393277e7d2",
+    "url": "/static/js/2.a3037c5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a3037c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab422193c9d9ecc0c39",
+    "url": "/static/js/main.4f75afd1.chunk.js"
+  },
+  {
+    "revision": "7f376e4c921fea4a31fe",
+    "url": "/static/js/runtime-main.39a3bea6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
